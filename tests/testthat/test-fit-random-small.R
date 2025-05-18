79892dc7599baf571fa15ddeae0f7eb328c0911a
Joe Thorley <joe@poissonconsulting.ca>
2025-05-18 12:32:17 +0800

    fix dists to dists

--- tests/testthat/test-fit-random-small.R
+++ tests/testthat/test-fit-random-small.R
@@ -3,7 +3,7 @@ test_that("fit random lnorm distributions with sample size 6 and pgtol 1e-8", {
   withr::with_seed(42, {
       for(i in 1:10^4) {
         data <- data.frame(Conc = ssd_rlnorm(6))
-        fit <- ssd_fit_dists(data = data, dist = "lnorm")
+        fit <- ssd_fit_dists(data = data, dists = "lnorm")
       }
   })
   expect_true(TRUE)
@@ -14,7 +14,7 @@ test_that("fit random lnorm distributions with sample size 6", {
   withr::with_seed(42, {
       for(i in 1:10^4) {
         data <- data.frame(Conc = ssd_rgamma(6))
-        fit <- ssd_fit_dists(data = data, dist = "lnorm")
+        fit <- ssd_fit_dists(data = data, dists = "lnorm")
       }
   })
   expect_true(TRUE)
