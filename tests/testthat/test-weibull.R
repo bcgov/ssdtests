f9e22252999eb979f5ebd888bc3644f8577ed063
Joe Thorley <joe@poissonconsulting.ca>
2025-08-30 07:34:49 -0700

    try seed on fit

--- tests/testthat/test-weibull.R
+++ tests/testthat/test-weibull.R
@@ -6,8 +6,9 @@ test_that("weibull fails to converge", {
   withr::with_seed(97, {
     data <- data.frame(Conc = ssdtools::ssd_rweibull(1000))
   })
-  skip_on_ci()
-  expect_warning(
+  withr::with_seed(97, {
+    expect_warning(
     fits <- ssdtools::ssd_fit_dists(data = data, dists = c("lnorm", "weibull")),
     "Distribution 'weibull' failed to converge \\(try rescaling data\\): ERROR: ABNORMAL_TERMINATION_IN_LNSRCH.")
+  })
 })
