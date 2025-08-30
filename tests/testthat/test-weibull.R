3325c21e65c8eef968528708c4d3926425ace1c5
Joe Thorley <joe@poissonconsulting.ca>
2025-08-30 07:33:59 -0700

    not a problem on windows!

--- tests/testthat/test-weibull.R
+++ tests/testthat/test-weibull.R
@@ -6,6 +6,7 @@ test_that("weibull fails to converge", {
   withr::with_seed(97, {
     data <- data.frame(Conc = ssdtools::ssd_rweibull(1000))
   })
+  skip_on_ci()
   expect_warning(
     fits <- ssdtools::ssd_fit_dists(data = data, dists = c("lnorm", "weibull")),
     "Distribution 'weibull' failed to converge \\(try rescaling data\\): ERROR: ABNORMAL_TERMINATION_IN_LNSRCH.")
